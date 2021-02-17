2cffd564a51a1933d684b9b89fd7f333ff84bd0f
Stefan <stefanG92@abv.bg>
2021-02-17 15:20:08 +0200

    Add more two task from this Lab

---
+++ Java-Basics/ConditionalStatementsAdvanced-Lab/05.SmallShop.js
@@ -0,0 +1,62 @@
+function thePrice(input) {
+    // : продукт (низ), град (низ) и количество 
+    let product = input[0];
+    let city = input[1];
+    let quantity = Number(input[2]);
+
+    // град / продукт	coffee	water	beer	sweets	peanuts
+    // Sofia	        0.50	0.80	1.20	1.45	1.60
+    // Plovdiv	        0.40	0.70	1.15	1.30	1.50
+    // Varna	        0.45	0.70	1.10	1.35	1.55
+
+    let totalPrice = 0;
+    switch (city) {
+        case "Sofia":
+            if (product === "coffee") {
+                totalPrice = quantity * 0.50;
+            } else if (product === "water") {
+                totalPrice = quantity * 0.80;
+            } else if (product === "beer") {
+                totalPrice = quantity * 1.20;
+            } else if (product === "sweets") {
+                totalPrice = quantity * 1.45;
+            } else if (product === "peanuts") {
+                totalPrice = quantity * 1.60;
+            }
+            break;
+        case "Plovdiv":
+            if (product === "coffee") {
+                totalPrice = quantity * 0.40;
+            } else if (product === "water") {
+                totalPrice = quantity * 0.70;
+            } else if (product === "beer") {
+                totalPrice = quantity * 1.15;
+            } else if (product === "sweets") {
+                totalPrice = quantity * 1.30;
+            } else if (product === "peanuts") {
+                totalPrice = quantity * 1.50;
+            }
+            break;
+        case "Varna":
+            if (product === "coffee") {
+                totalPrice = quantity * 0.45;
+            } else if (product === "water") {
+                totalPrice = quantity * 0.70;
+            } else if (product === "beer") {
+                totalPrice = quantity * 1.10;
+            } else if (product === "sweets") {
+                totalPrice = quantity * 1.35;
+            } else if (product === "peanuts") {
+                totalPrice = quantity * 1.55;
+            }
+            break;
+
+    }
+    console.log(totalPrice)
+}
+thePrice(["peanuts",
+"Plovdiv",
+"1"]);
+
+
+
