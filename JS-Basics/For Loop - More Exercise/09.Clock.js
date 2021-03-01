24aafed73cb1820afbc5f543c5628be12d18c5c1
Stefan <stefanG92@abv.bg>
2021-03-01 16:30:52 +0200

    Added task clock

---
+++ JS-Basics/For Loop - More Exercise/09.Clock.js
@@ -0,0 +1,12 @@
+function oclock (){
+
+    for (let i = 0; i <= 23; i++) {
+        
+        for (let j = 0; j <= 59; j++) {
+            console.log(`${i} : ${j}`);
+            
+        }
+        
+    }
+}
+oclock();
