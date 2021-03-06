6dfb6369030087bc82a9b41a27f7b660c2914857
Stefan <78791572+stefanG92@users.noreply.github.com>
2021-03-06 20:35:36 +0200

    Update 06.MaxNumber.js
    I changed let maxNumber with other method

--- JS-Basics/While Loop - Lab/06.MaxNumber.js
+++ JS-Basics/While Loop - Lab/06.MaxNumber.js
@@ -2,7 +2,7 @@ function findMaxNumber (input){
     let index = 0;
     let command = input[index];
 
-    let maxNumber = Number.MIN_VALUE;
+    let maxNumber = Number.MIN_SAFE_INTEGER;
 
     while(command !== "Stop"){
         let number = Number (command);
